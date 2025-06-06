fa5dc690c2fbc1a6013483744a35b731e96bd16f
jlee1028 <jasonlee1028@yahoo.com>
2025-06-06 16:35:57 -0700

    update requirements

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -28,10 +28,7 @@ async def get_current_user(token: TokenDep) -> User:
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Could not validate credentials",
         )
-    print(token_data.sub)
-    print(type(token_data.sub))
     user = await User.find_one(User.user_id == token_data.sub)
-    print(user)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
